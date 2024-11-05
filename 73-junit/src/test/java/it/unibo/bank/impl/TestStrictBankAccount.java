1a6289852eef6865739ab6bf0e562932ddfd80ab
Giuseppe Fusco <giuseppe.fusco9@studio.unibo.it>
2024-11-05 18:12:31 +0100

    73 complete: no more warnings

--- 73-junit/src/test/java/it/unibo/bank/impl/TestStrictBankAccount.java
+++ 73-junit/src/test/java/it/unibo/bank/impl/TestStrictBankAccount.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.Assertions;
 
 
 /**
